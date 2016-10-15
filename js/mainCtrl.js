d43373a2f035567d193fa9a4046eace46f43dc1b
ToasterTom <tate.thomas5595@gmail.com>
2016-10-15 11:37:42 -0600

    Fixed api character search

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,5 +1,11 @@
 angular.module('heroSearch').controller('mainCtrl', function ($scope, serviceHttp) {
 
-$scope.characters = serviceHttp.getCharacter();
+// $scope.characters = serviceHttp.getCharacter();
+
+  $scope.getCharacterData = function (character) {
+    serviceHttp.getCharacter(character).then(response) {
+      $scope.characterData = response;
+    }
+  }
 
 })
--- js/serviceHttp.js
+++ js/serviceHttp.js
@@ -1,18 +1,33 @@
 angular.module('heroSearch').service('serviceHttp', function ($http, $q) {
 
 this.getCharacter = function (character) {
+  var defer = $q.defer();
   $http({
     method: 'GET',
     url: 'https://gateway.marvel.com/v1/public/characters',
     params: {
-    "apikey": "75f03c4b1cd6f77e464461c42f03c068"
+    "apikey": "75f03c4b1cd6f77e464461c42f03c068",
+    "name": character
      }
   }).then(function (response) {
     console.log(response);
+
     var filterCharData = response.data.data.results;
     console.log(filterCharData);
     var responseData = [];
+    for (var i = 0; i < filterCharData.length; i++){
+
+      var obj = {
+        thumbnail: filterCharData[i].thumbnail,
+        name: filterCharData[i].name,
+        series: filterCharData[i].series,
+        description: filterCharData[i].description
+      }
+      responseData.push(obj);
+    }
+    defer.resolve(responseData);
   })
+return defer.promise;
 }
 
 })
