cd10a23b0a5ad1b1aaa10239c324b4e3ef23539c
ToasterTom <tate.thomas5595@gmail.com>
2016-10-15 09:17:20 -0600

    Added Heading

--- js/serviceHttp.js
+++ js/serviceHttp.js
@@ -7,8 +7,6 @@ this.getCharacter = function (character) {
     url: 'https://gateway.marvel.com/v1/public/comics',
     params: {
     "apikey": "75f03c4b1cd6f77e464461c42f03c068"
-    // "ts": myTs,
-    // "hash": "md5(ts+3409f516b08d6b34a919f5871fb7c4423faef01d+75f03c4b1cd6f77e464461c42f03c068)"
      }
   }).then(function (response) {
     console.log(response);
