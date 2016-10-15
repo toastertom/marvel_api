27d95830226bd8916373ab1f980f1b2bdb172e28
ToasterTom <tate.thomas5595@gmail.com>
2016-10-15 10:38:45 -0600

    Changed api call

--- js/serviceHttp.js
+++ js/serviceHttp.js
@@ -3,7 +3,7 @@ angular.module('heroSearch').service('serviceHttp', function ($http, $q) {
 this.getCharacter = function (character) {
   $http({
     method: 'GET',
-    url: 'https://gateway.marvel.com/v1/public/comics',
+    url: 'https://gateway.marvel.com/v1/public/characters',
     params: {
     "apikey": "75f03c4b1cd6f77e464461c42f03c068"
      }
@@ -11,6 +11,7 @@ this.getCharacter = function (character) {
     console.log(response);
     var filterCharData = response.data.data.results;
     console.log(filterCharData);
+    var responseData = [];
   })
 }
 
