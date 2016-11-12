bb9eaefc65270cfbb02c1e74a2dbc24cfc6902fb
ToasterTom <tate.thomas5595@gmail.com>
2016-11-11 17:26:28 -0700

    using ui router

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-angular.module('heroSearch', ['ui.router'])
+angular.module('heroSearch', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home',{
+                url:'/',
+                templateUrl: "../views/homeTmpl.html",
+                controller:'mainCtrl'
+            })
+            .state('characters',{
+                url:'/characters',
+                templateUrl: "../views/characterCardsTmpl.html",
+                controller:'mainCtrl'
+            })
+
+            $urlRouterProvider
+              .otherwise('/');
+
+            });
