a45e4eebf534a3b41d1182f7c509ba6e979c7da4
ToasterTom <tate.thomas5595@gmail.com>
2016-11-11 17:43:20 -0700

    ui router scrapped

--- js/app.js
+++ js/app.js
@@ -1,17 +1,1 @@
-angular.module('heroSearch', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('home',{
-                url:'/',
-                templateUrl: "../views/homeTmpl.html",
-                controller:'mainCtrl'
-            })
-            .state('characters',{
-                url:'/characters',
-                templateUrl: "../views/characterCardsTmpl.html",
-                controller:'mainCtrl'
-            })
-
-            $urlRouterProvider
-              .otherwise('/');
-
-            });
+angular.module('heroSearch', [])
