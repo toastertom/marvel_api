12635deefc88bb1b214a2881835e8cf9e46f7d1a
ToasterTom <tate.thomas5595@gmail.com>
2016-12-19 21:27:39 -0700

    directive adjustment

--- js/directives/defaultImageCardDirect.js
+++ js/directives/defaultImageCardDirect.js
@@ -3,7 +3,7 @@ app = angular.module('heroSearch')
 app.directive('scroll', function ($window) {
     return function(scope, element, attrs) {
         angular.element($window).bind('scroll', function() {
-             if (this.pageYOffset >= 300) {
+             if (this.pageYOffset >= 200) {
                 element.addClass('hidden');
                 //element.removeClass('card');
               }  else {
