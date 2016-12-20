6a7639b61e6156d37c3380a1586b557d1434c23f
ToasterTom <tate.thomas5595@gmail.com>
2016-12-19 21:08:01 -0700

    Trouble Shooting

--- js/directives/defaultImageCardDirect.js
+++ js/directives/defaultImageCardDirect.js
@@ -2,13 +2,13 @@ app = angular.module('heroSearch')
 app.directive('hidden', function ($window) {
     return function(scope, element, attrs) {
         angular.element($window).bind('hidden', function() {
-             if (this.pageYOffset >= 400) {
+             if (this.pageYOffset >= 300) {
                 element.addClass('hidden');
-                element.removeClass('card');
+                //element.removeClass('card');
               }  else {
                 //  scope.boolChangeClass = false;
-                element.removeClass('hidden');
-                element.addClass('card');
+                //element.removeClass('hidden');
+                //element.addClass('card');
              }
 
         });
